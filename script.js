19d182f2eaccb6befbe6ba2a788320da0de7a5af
Anmol-G-K <cb.en.u4eee23103@cb.students.amrita.edu>
2024-06-16 20:33:16 +0530

    pop up fixes

--- script.js
+++ script.js
@@ -15,31 +15,32 @@ function openModal(title, camera, shutterSpeed, aperture, iso, description) {
     var modal = document.getElementById('modal');
     var modalImage = document.getElementById('modal-image');
     var modalTitle = document.getElementById('modal-title');
-    var modalDescription = document.getElementById('modal-description');
     var modalCamera = document.getElementById('modal-camera');
     var modalShutterSpeed = document.getElementById('modal-shutter-speed');
     var modalAperture = document.getElementById('modal-aperture');
     var modalIso = document.getElementById('modal-iso');
 
-    modalImage.src = event.target.src;
-    modalTitle.textContent = title;
-    modalDescription.textContent = description;
-    modalCamera.textContent = camera;
-    modalShutterSpeed.textContent = shutterSpeed;
-    modalAperture.textContent = aperture;
-    modalIso.textContent = iso;
+    if (modal && modalTitle && modalCamera && modalShutterSpeed && modalAperture && modalIso) {
+        modalImage.src = event.target.src;
+        modalTitle.textContent = title;
+        modalCamera.textContent = "Camera: " + camera;
+        modalShutterSpeed.textContent = "Shutter Speed: " + shutterSpeed;
+        modalAperture.textContent = "Aperture: " + aperture;
+        modalIso.textContent = "ISO: " + iso;
 
-    modal.style.display = 'block';
-    document.body.classList.add('modal-open'); // Add class to body for background blur effect
+        modal.style.display = 'block';
+        document.body.classList.add('modal-open'); // Add class to body for background blur effect
+    } else {
+        console.error('One or more modal elements not found.');
+    }
 }
 
-
-// Function to close modal
 function closeModal() {
     var modal = document.getElementById('modal');
     modal.style.display = 'none';
     document.body.classList.remove('modal-open'); // Remove class from body to remove background blur effect
 }
+
 
 // Close modal if user clicks outside the modal content
 window.onclick = function(event) {
